f6fdfa5b9698452fd9d4590f6a68d4b5f69620a4
Andrew <oreljs13@gmail.com>
2018-09-05 22:09:52 +0300

    add array to constructor for identification elements, and add him position on grid

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ const HEIGHT = window.innerHeight;
 
 class Game {
     constructor() {
+        this.elements = [];
         this._initApp();
         //Counts elements on grid
-        this._drawElementsOnGrid(6, 6);
+        this._drawElementsOnGrid(5, 5);
     }
 
     _initApp() {
@@ -18,27 +19,35 @@ class Game {
         let y = 0;
 
         //Draw elements on Y coordinates
-        for (let i = 1; i <= width; i++) {
+        for (let i = 0; i <= width; i++) {
             //Draw elements on X coordinates            
-            for (let j = 1; j <= height; j++) {
-                createSprite(this.app, {
+            for (let j = 0; j <= height; j++) {
+                const type = randomInteger(1, 4);
+                const sprite = createSprite(this.app, {
                     x: x,
                     y: y,
                     width: 64,
                     height: 64,
                     interactive: true,
-                    path: `images/${randomInteger(1,4)}.png`
+                    path: `images/${type}.png`
                 });
 
+                sprite.typeId = type;
+                sprite.uniqId = Math.random().toString(16).slice(2);
+                sprite.columnPosition = i;
+                sprite.rawPosition = j;
+
+                this.elements.push(sprite);
+
                 if (x !== width) {
-                    x += 64;
+                    x += 66;
                 }
             }
 
             x = 0;
 
             if (i !== height) {
-                y += 64;
+                y += 66;
             }
 
             createSprite(this.app, {
@@ -50,6 +59,8 @@ class Game {
                 path: `images/${randomInteger(1,4)}.png`
             });
         }
+
+        console.log(this.elements);
     }
 }
 
