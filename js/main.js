50a052484a0d4f2b3324aa5d20579ddd7d53630f
mcurrr <technosigal@gmail.com>
2018-09-10 01:24:55 +0300

    delete don't used code and add third argument to constructor

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@ const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 
 class Game {
-    constructor(rowsCount, columnsCount) {
+    constructor(rowsCount, columnsCount, countsTypes) {
         this._rowsCount = rowsCount;
         this._columnsCount = columnsCount;
+        this._countsTypes = countsTypes;
         this.elements = [];
         this.initApp();
-        //Counts elements on grid
         this.drawElementsOnGrid();
-        this.createButtons(this.app);
     }
 
     initApp() {
@@ -35,56 +34,6 @@ class Game {
 
         container.x = (WIDTH / 2) - ((columns * 66) / 2);
         container.y = (HEIGHT / 2) - ((rows * 66) / 2);
-    }
-
-    createElement(app, options) {
-            options = Object.assign({
-                x: 0,
-                y: 0,
-                width: 0,
-                height: 0,
-                interactive: true
-            }, options);
-
-            const sprite = PIXI.Sprite.fromImage(options.path);
-
-            sprite.width = options.width;
-            sprite.height = options.height;
-            sprite.x = options.x;
-            sprite.y = options.y;
-
-            app.stage.addChild(sprite);
-
-            return sprite;
-    }
-
-    createButtons(app) {
-        const refreshButton = this.createElement(app, {
-                x: x,
-                y: y,
-                width: 64,
-                height: 64,
-                path: 'images/btn-refresh.png'
-            });
-
-            const deleteButton = createElement(app, {
-                x: x,
-                y: y,
-                width: 64,
-                height: 64,
-                path: 'images/btn-delete.png.png'
-            });
-
-        refreshButton.on("click", () => {
-            console.log("refreshButton");
-        });
-
-        deleteButton.on("click", () => {
-            console.log("deleteButton");
-        });
-
-        app.stage.addChild(refreshButton);
-        app.stage.addChild(deleteButton);
     }
 
     checkElements(element) {
@@ -152,7 +101,7 @@ class Game {
     // }
 
     createSprite(container, x, y) {
-        const type = randomInteger(1, 2);
+        const type = randomInteger(1, this._countsTypes);
         const sprite = PIXI.Sprite.fromImage(`images/${type}.png`);
         const spriteSize = 64;
         const spriteBorder = 2;
@@ -178,7 +127,6 @@ class Game {
 
     setAttribute(column, row, element, type) {
         element.typeId = type;
-        element.uniqId = Math.random().toString(16).slice(2);
         element.xPosition = column;
         element.yPosition = row;
 
@@ -196,10 +144,4 @@ function randomInteger(min, max) {
     return rand;
 };
 
-function toPercent(x, y) {
-    const pxX = WIDTH * x / 100;
-    const pxY = HEIGHT * y / 100;
-        return { x: pxX, y: pxY };
-}
-
-new Game(8, 12);
+new Game(7, 9, 3);
